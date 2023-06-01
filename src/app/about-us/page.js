148bc40f6f102246032f642c6a2a7d345f2e6c26
Nafsirudin <nafsirudin8gmail.com>
2023-06-01 22:31:06 +0700

    next config dan buat halaman aboutus

---
+++ src/app/about-us/page.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'About Us',
+  description: 'Halaman About Us',
+}
+
+const AboutUsPage = () => {
+  return (
+    <div>About Us Page</div>
+  )
+}
+
+export default AboutUsPage
